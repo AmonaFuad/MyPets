0fd442c3dee80e34de92b95c3edb418add8c188c
Insaf Fuad <insaffofo@gmail.com>
2020-05-14 23:34:11 +0300

    my pet lesson 3

--- app/src/main/java/com/example/android/mypets/activities/CatalogActivity.java
+++ app/src/main/java/com/example/android/mypets/activities/CatalogActivity.java
@@ -3,10 +3,12 @@ package com.example.android.mypets.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -33,7 +35,8 @@ public class CatalogActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+               Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+
                 startActivity(intent);
             }
         });
@@ -41,7 +44,7 @@ public class CatalogActivity extends AppCompatActivity {
 
 
     }
-    // we add this method to refrish the list with new pet in the database
+    // we add this method to refresh the list with new pet in the database
     @Override
     protected void onStart() {
         super.onStart();
@@ -55,43 +58,53 @@ public class CatalogActivity extends AppCompatActivity {
     private void displayDatabaseInfo() {
         // To access our database, we instantiate our subclass of SQLiteOpenHelper
         // and pass the context, which is the current activity.
-        PetDbHelper mDbHelper = new PetDbHelper(this);
-
-        // Create and/or open a database to read from it
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        // Perform this raw SQL query "SELECT * FROM pets"
-        // to get a Cursor that contains all rows from the pets table.
-
-        String[] projection = {PetContract.PetEntry.COLUMN_PET_BREED,
-               PetContract.PetEntry.COLUMN_PET_WEIGHT };
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                PetContract.PetEntry._ID,
+                PetContract.PetEntry.COLUMN_PET_NAME,
+                PetContract.PetEntry.COLUMN_PET_BREED,
+                PetContract.PetEntry.COLUMN_PET_GENDER,
+                PetContract.PetEntry.COLUMN_PET_WEIGHT };
         String selection = PetContract.PetEntry.COLUMN_PET_GENDER + "=?";
         String []selectionArgs = new String[] {String.valueOf(PetContract.PetEntry.GENDER_MALE)};
 
-        Cursor cursor = db.query(PetContract.PetEntry.TABLE_NAME, projection,
-                selection, selectionArgs,
-                null, null, null);
-
-        try {
-            // Display the number of rows in the Cursor (which reflects the number of rows in the
-            // pets table in the database).
-            TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+        // Perform a query on the pets table
+        Cursor cursor =getContentResolver().query(PetContract.PetEntry.CONTENT_URI, projection,
+                selection, selectionArgs,null,null);
+               TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+                  try {
             displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
-            displayView.append(PetContract.PetEntry.COLUMN_PET_BREED + "    -   " +
-                    PetContract.PetEntry.COLUMN_PET_WEIGHT+ "\n");
-            // Figure out the index of each column
-            int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
-            int weightColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_WEIGHT);
-            // Use that index to extract the String or Int value of the word
-            // at the current row the cursor is on.
-            while (cursor.moveToNext()){
+            displayView.append(PetContract.PetEntry._ID + " - " +
+                    PetContract.PetEntry.COLUMN_PET_NAME + " - " +
+                    PetContract.PetEntry.COLUMN_PET_BREED + " - " +
+                    PetContract.PetEntry.COLUMN_PET_GENDER + " - " +
+                    PetContract.PetEntry.COLUMN_PET_WEIGHT + "\n");
 
-             String breedString =  cursor.getString(breedColumnIndex);
-             String weightString=cursor.getString(weightColumnIndex);
-                // Display the values from each column of the current row in the cursor in the TextView
-                displayView.append(("\n" + breedString + "        -     " +
-                        weightString));
-            }
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(PetContract.PetEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
+            int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+            int genderColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_GENDER);
+            int weightColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_WEIGHT);
+
+
+                      // Iterate through all the returned rows in the cursor
+                      while (cursor.moveToNext()) {
+                          // Use that index to extract the String or Int value of the word
+                          // at the current row the cursor is on.
+                          int currentID = cursor.getInt(idColumnIndex);
+                          String currentName = cursor.getString(nameColumnIndex);
+                          String currentBreed = cursor.getString(breedColumnIndex);
+                          int currentGender = cursor.getInt(genderColumnIndex);
+                          int currentWeight = cursor.getInt(weightColumnIndex);
+                          // Display the values from each column of the current row in the cursor in the TextView
+                          displayView.append(("\n" + currentID + " - " +
+                                  currentName + " - " +
+                                  currentBreed + " - " +
+                                  currentGender + " - " +
+                                  currentWeight));
+                      }
 
         } finally {
             // Always close the cursor when you're done reading from it. This releases all its
@@ -109,14 +122,11 @@ public class CatalogActivity extends AppCompatActivity {
         values.put(PetContract.PetEntry.COLUMN_PET_BREED, "Tabby");
         values.put(PetContract.PetEntry.COLUMN_PET_GENDER, PetContract.PetEntry.GENDER_MALE);
         values.put(PetContract.PetEntry.COLUMN_PET_WEIGHT, 7);
-
-        PetDbHelper mDbHelper = new PetDbHelper(this);
-
-        // Create and/or open a database to read from it
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-        long newRowId =db.insert(PetContract.PetEntry.TABLE_NAME, null, values);
-        Log.v("catalg activity", "new row id "+newRowId);
+        Uri newUri = getContentResolver().insert(PetContract.PetEntry.CONTENT_URI, values);
+        Log.v("catalg activity", "new row id "+newUri);
+        Toast.makeText(this, "hiiiiiiii" + newUri, Toast.LENGTH_SHORT).show();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
